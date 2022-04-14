c56f540ff23de4549cee04fa8fb38a1f01e3accf
Raimon Esteve <raimonesteve@gmail.com>
2022-04-14 12:34:59 +0200

    Check rule only if _check_access is set and enforce companies rule issue4080

--- tests/test_account_bank_statement_counterpart.py
+++ tests/test_account_bank_statement_counterpart.py
@@ -7,9 +7,10 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class AccountBankStatementCounterpartTestCase(ModuleTestCase):
+class AccountBankStatementCounterpartTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Account Bank Statement Counterpart module'
     module = 'account_bank_statement_counterpart'
 
